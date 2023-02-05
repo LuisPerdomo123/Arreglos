1db81ce1aa78c04bb8083d65578c98462f0c65ed
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-05 07:09:58 -0500

    Arreglos parte I

---
+++ Arreglos/src/test/TestArreglos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author lepo9
+ */
+public class TestArreglos {
+    public static void main(String[] args) {
+        int edades[] = new int [3];//Con la sintaxis [] se determina que es un arreglo. Puede indicarse antes o despues del nombre de la variable
+        /*
+        Un arreglo es un objeto y hereda de la clase Object.
+        Siempre se debe especificar el tipo y longitud del arreglo.
+        Una vez se define el espacio del arreglo, este no puede crecer mas.
+        */
+        System.out.println("edades = " + edades);
+        
+    }
+}
